92aafd472f1699c43ac895f4decb4c5b87c41ced
Sascha Mann <SaschaMann@users.noreply.github.com>
2016-10-15 17:20:26 +0200

    Added 5s delay after starting a round

--- triviabot/mod_help.py
+++ triviabot/mod_help.py
@@ -5,24 +5,28 @@ command_dict = {}
 
 def on_load(bot):
     bot.add_command('help', helpme)
-    command_help('help', 'displays this message.')
+    add_helptext('help', 'displays this message.')
 
 
 def helpme(bot, user, channel, args):
-    message = '\nThe following commands are available: '
+    message = 'The following commands are available: '
+
     # Generates a list of available commands
     for command in command_dict:
         message += command + ' '
+
     # Sends a description of each command
     for command in command_dict:
         message += '\n' + command + ': ' + command_dict.get(command)
+
     # Copyright and stuff
-    message += '\n\nThis product is not endorsed, certified or otherwise approved in any way by Riot Games, Inc. or any of its affiliates.'
+    message += '\nThis product is not endorsed, certified or otherwise approved in any way by Riot Games, Inc. or any of its affiliates.'
+
     # Send message to user
     nickname = separate_name(user)
     bot.send_msg(nickname[0], message)
 
 
-def command_help(command, helptext):
-    """Generate a entry in the command_dict dictionary that gives info about the command and it's usage when 'helpme' is used."""
+def add_helptext(command, helptext):
+    """Adds info about the command to be displayed by the help command."""
     command_dict[command] = helptext
--- triviabot/mod_trivia.py
+++ triviabot/mod_trivia.py
@@ -1,11 +1,12 @@
-import time
 import random
+import threading
 
 import riotwatcher
-from riotwatcher import RateLimit
+from riotwatcher import RateLimit, EUROPE_WEST
 
 from content.questions import question_generators
 from config import config
+from mod_help import add_helptext
 
 
 watcher = ''
@@ -13,18 +14,24 @@ watcher = ''
 
 def on_load(bot):
     bot.add_command('trivia', trivia)
+    add_helptext('trivia', 'Stars a new round of trivia.')
 
     global watcher
-    watcher = riotwatcher.RiotWatcher(config['api_key'], default_region=riotwatcher.EUROPE_WEST,
+    watcher = riotwatcher.RiotWatcher(config['api_key'], default_region=EUROPE_WEST,
                                       limits=(RateLimit(10, 10), RateLimit(500, 600), ))
 
 
 def trivia(bot, user, channel, args):
     """Starts a new round of trivia."""
-    global watcher
+    # Notify the channel that someone started a new round
+    bot.send_msg(channel, '%s has started a new trivia round! Get ready!' % user)
+
+    # Prepare the question
     question = random.choice(question_generators)(watcher)
 
     event = bot.Event(question.answer, channel, question.solve_question)
     bot.add_event(event)
 
-    bot.send_msg(channel, question.question)
+    # Wait 5s before showing the question
+    timer = threading.Timer(5.0, bot.send_msg, args=[channel, question.question])
+    timer.start()
