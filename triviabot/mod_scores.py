21eb7fa255e63905f9d418dd39bf11a431df30e3
Sascha Mann <SaschaMann@users.noreply.github.com>
2016-11-14 18:33:36 +0100

    Added score keeping using sqlite3 and SQLAlchemy

--- triviabot/mod_scores.py
+++ triviabot/mod_scores.py
@@ -4,6 +4,8 @@ from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from functools import partial
+
 from utilities import separate_name
 
 Base = declarative_base()
@@ -11,7 +13,7 @@ Base = declarative_base()
 
 def on_load(bot):
     bot.add_command('top', top)
-    bot.add_command('myscore', myscore)
+    bot.add_command('myscore', partial(score, self_score=True))
     bot.add_command('score', score)
 
 
@@ -26,7 +28,8 @@ class User(Base):
         return '<User(name=\'%s\', score=\'%s\')>' % (self.name, self.score)
 
 # Init DB
-engine = create_engine('sqlite:///trivia.db', echo=True)  # TODO Move db file path to config
+# TODO Move db file path to config, currently shared across all channels
+engine = create_engine('sqlite:///trivia.db', echo=True)
 Base.metadata.create_all(engine)  # TODO Once DB is out of memory, this needs to go into a seperate init_db.py file
 Session = sessionmaker(bind=engine)
 
@@ -47,7 +50,7 @@ def session_scope():
 
 
 def get_user(session, nick):
-    return session.query(User).filter_by(name=nick).first()
+    return session.query(User).filter(User.name.ilike(nick)).first()
 
 
 def create_user(session, nick):
@@ -62,24 +65,33 @@ def update_score(session, nick, add_score):
 # Commands
 def top(bot, user, channel, args):
     with session_scope() as session:
-        users = session.query(User).all()
+        if args:
+            n = args[0]
+        else:
+            n = 10
 
-        for user in users:
-            bot.send_msg(channel, str(user))
+        users = session.query(User).order_by(User.score.desc()).all()
+
+        msg = ''
+        for i, user in enumerate(users):
+            msg += '#{rank} {nick} ({score}) ||'.format(rank=i+1, nick=user.name, score=user.score)
+
+            if i > n:
+                break
+
+        bot.send_msg(channel, msg[:-2])
 
 
-def score(bot, user, channel, args):
-    pass
-
-
-def myscore(bot, user, channel, args):
-    nick, identifier, hostname = separate_name(user)
+def score(bot, user, channel, args, self_score=False):
+    if not args or self_score:
+        nick = separate_name(user)[0]
+    else:
+        nick = args[0]
 
     with session_scope() as session:
         user = get_user(session, nick)
 
         if not user:
-            bot.send_msg(channel, 'Your score is 0. Everyone point and laugh at %s.' % nick)
+            bot.send_msg(channel, '{nick} has not played trivia yet.'.format(nick=user.name))
         else:
-            _score = str(user.score)
-            bot.send_msg(channel, 'Your score is %s' % _score)
+            bot.send_msg(channel, '{nick}\'s score is {score}'.format(nick=user.name, score=user.score))
